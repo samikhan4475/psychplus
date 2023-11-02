fa704ca8a076d7765849863d56ba4029ba214d6f
Arya Mohades <aryamohades@gmail.com>
2023-11-02 10:46:19 -0600

    Task: move EHR to clinical team (#38)

--- apps/ehr/src/app/error.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-
-import { RootError } from '@psychplus/components'
-
-export default RootError
--- apps/ehr/src/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { type Metadata } from 'next'
-import { RootLayout } from '@psychplus/components'
-
-export const metadata: Metadata = {
-  title: 'title',
-  description: 'description',
-}
-
-export default RootLayout
