858b7a6e5a2f0f22a7ee0d6b998e8e0968ec2d68
Arya Mohades <aryamohades@gmail.com>
2024-01-13 13:46:52 -0700

    Task: Convert datetime from API to users local time (#109)

--- packages/utils/src/time/index.ts
+++ packages/utils/src/time/index.ts
@@ -5,18 +5,24 @@ const daysAgo = (days: number) =>
   new Date(new Date().setDate(new Date().getDate() - days))
 
 const formatDate = (date: Date) => {
+  const d = new Date(date)
+  d.setMinutes(d.getMinutes() - d.getTimezoneOffset())
+
   return new Intl.DateTimeFormat('en-US', {
     year: '2-digit',
     month: 'numeric',
     day: 'numeric',
-  }).format(date)
+  }).format(d)
 }
 
 const formatTime = (date: Date) => {
-  let hours: number | string = date.getHours()
+  const d = new Date(date)
+  d.setMinutes(d.getMinutes() - d.getTimezoneOffset())
+
+  let hours: number | string = d.getHours()
   hours = hours < 10 ? `0${hours}` : hours
 
-  let minutes: number | string = date.getMinutes()
+  let minutes: number | string = d.getMinutes()
   minutes = minutes < 10 ? `0${minutes}` : minutes
 
   return `${hours}:${minutes}`
