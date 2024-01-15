61c4d85987410cca8a732d016004dd35960f3368
Arya Mohades <aryamohades@gmail.com>
2024-01-15 09:47:43 -0700

    Bugfix: if app path is empty, use undefined assetPrefix (#111)
    Fixes invalid static asset URLs in non dev environments

--- apps/patient/next.config.js
+++ apps/patient/next.config.js
@@ -3,7 +3,9 @@ const config = {
   assetPrefix:
     process.env.NODE_ENV !== 'development' &&
     process.env.NEXT_PUBLIC_DISABLE_APP_PATH !== 'true'
-      ? `/${process.env.NEXT_PUBLIC_APP_PATH}/`
+      ? process.env.NEXT_PUBLIC_APP_PATH
+        ? `/${process.env.NEXT_PUBLIC_APP_PATH}/`
+        : undefined
       : undefined,
   reactStrictMode: true,
   swcMinify: true,
