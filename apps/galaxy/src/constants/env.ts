32502ca84b7035a36e6eb75d16c0cfe5e0d62d40
Arya Mohades <aryamohades@gmail.com>
2024-08-23 11:05:04 -0600

    update and fix some envs (#434)

--- apps/galaxy/src/constants/env.ts
+++ apps/galaxy/src/constants/env.ts
@@ -4,7 +4,7 @@ type AppEnv = 'development' | 'staging' | 'production'
 const APP_CODE = process.env.APP_CODE ?? 'p+ui'
 const APP_VERSION = process.env.APP_VERSION ?? '1.0.0'
 const APP_ENV = (process.env.APP_ENV ?? 'development') as AppEnv
-const ROOT_DOMAIN = process.env.ROOT_DOMAIN ?? 'localhost'
+const ROOT_DOMAIN = process.env.ROOT_DOMAIN ?? undefined
 
 // PsychPlus service URLs
 const API_URL = process.env.API_URL ?? ''
@@ -17,17 +17,17 @@ const SCRIPTSURE_PLATFORM_URL = process.env.SCRIPTSURE_PLATFORM_URL ?? ''
 const SCRIPTSURE_APP_URL = process.env.SCRIPTSURE_APP_URL ?? ''
 
 // Cookies
-const USER_COOKIE = process.env.USER_COOKIE ?? 'p+_user'
-const ACCESS_TOKEN_COOKIE = process.env.ACCESS_TOKEN_COOKIE ?? 'p+_access_token'
+const USER_COOKIE = process.env.USER_COOKIE ?? 'p+_staff_user'
+const ACCESS_TOKEN_COOKIE = process.env.ACCESS_TOKEN_COOKIE ?? 'p+_staff_access_token'
 const ACCESS_TOKEN_EXPIRY_COOKIE =
-  process.env.ACCESS_TOKEN_EXPIRY_COOKIE ?? 'p+_access_token_expiry'
+  process.env.ACCESS_TOKEN_EXPIRY_COOKIE ?? 'p+_staff_access_token_expiry'
 const REFRESH_TOKEN_COOKIE =
-  process.env.REFRESH_TOKEN_COOKIE ?? 'p+_refresh_token'
+  process.env.REFRESH_TOKEN_COOKIE ?? 'p+_staff_refresh_token'
 const SCRIPTSURE_SESSION_COOKIE =
-  process.env.SCRIPTSURE_SESSION_COOKIE ?? 'p+_scriptsure_session'
+  process.env.SCRIPTSURE_SESSION_COOKIE ?? 'p+_staff_scriptsure_session'
 const SCRIPTSURE_SESSION_CREATED_AT_COOKIE =
   process.env.SCRIPTSURE_SESSION_CREATED_AT_COOKIE ??
-  'p+_scriptsure_session_created_at'
+  'p+_staff_scriptsure_session_created_at'
 
 export {
   APP_CODE,
