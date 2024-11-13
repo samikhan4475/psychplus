22ad3d569dbded47d777b9effed212fd413230ff
Ali Hamza <ali.hamza@psychplus.com>
2024-11-13 23:50:53 +0500

    Ehr 10285 patient portal disable scriptsure api (#734)

--- apps/patient/src/features/medications/ui/active-medications-table/active-medications-table.server.tsx
+++ apps/patient/src/features/medications/ui/active-medications-table/active-medications-table.server.tsx
@@ -1,8 +1,5 @@
-import { getDrugHistory } from '@psychplus-v2/scriptsure/api'
 import { withSuspense } from '@psychplus-v2/utils'
-import { getProfile } from '@/api'
 import { CardContainer, LoadingPlaceholder } from '@/components-v2'
-import { getActiveMedsFromDrugHistory } from '../../utils'
 import { ActiveMedicationsTable as Client } from './active-medications-table'
 
 type ActiveMedicationsTableServerProps = Omit<
@@ -13,15 +10,15 @@ type ActiveMedicationsTableServerProps = Omit<
 const ActiveMedicationsTableServer = async (
   props: ActiveMedicationsTableServerProps,
 ) => {
-  const profile = await getProfile()
-  if (profile.state === 'error') {
-    throw new Error(profile.error)
-  }
+  // const profile = await getProfile()
+  // if (profile.state === 'error') {
+  //   throw new Error(profile.error)
+  // }
 
-  const drugHistory = await getDrugHistory(profile.data)
-  const activeMedications = getActiveMedsFromDrugHistory(drugHistory)
+  // const drugHistory = await getDrugHistory(profile.data)
+  // const activeMedications = getActiveMedsFromDrugHistory(drugHistory)
 
-  return <Client data={activeMedications} {...props} />
+  return <Client data={[]} {...props} />
 }
 
 const ActiveMedicationsTable = withSuspense(ActiveMedicationsTableServer, {
