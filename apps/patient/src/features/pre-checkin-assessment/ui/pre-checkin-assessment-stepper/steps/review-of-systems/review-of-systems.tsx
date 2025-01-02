7047b96c7d25a926e3a9fd31e5f21992d07dc5f4
Ali Hamza <ali.hamza@psychplus.com>
2025-01-02 18:12:24 +0500

    Ehr 9670 develop review of systems UI for pre checking assessment form (#808)
    Co-authored-by: Salman Rana <161335750+salman-rana-psychplus@users.noreply.github.com>

--- apps/patient/src/features/pre-checkin-assessment/ui/pre-checkin-assessment-stepper/steps/review-of-systems/review-of-systems.tsx
+++ apps/patient/src/features/pre-checkin-assessment/ui/pre-checkin-assessment-stepper/steps/review-of-systems/review-of-systems.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box, Button, Flex, Text } from '@radix-ui/themes'
+import ReviewOfSystemsSection from './blocks/review-of-systems-section'
 
-const ReviewOfSystems = () => {
-  return <div>ReviewOfSystems</div>
+const ReviewOfSystems: React.FC = () => {
+  return (
+    <Box className="bg-white rounded-2 border border-[#D9E2FC] p-6 pb-8">
+      <Flex align="center" className="mb-4 flex-col gap-[19px] sm:flex-row">
+        <Text className="text-black text-[32px] font-[600]  leading-8">
+          Review of Systems
+        </Text>
+        <Button
+          color="blue"
+          variant="outline"
+          className="bg-white h-[28px] content-center rounded-[100px] border border-[#194595] px-3 text-[12px] font-[600] text-[#194595] "
+        >
+          No Concerns
+        </Button>
+      </Flex>
+      <Box className="gap:4 grid grid-cols-1 md:grid-cols-2 md:gap-6">
+        <ReviewOfSystemsSection />
+      </Box>
+    </Box>
+  )
 }
-
 export { ReviewOfSystems }
