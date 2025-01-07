1351a1fbb8ac073ce8d379491eda4afb0184d630
Zeeshan Haider <81483119+zeeeshan-haider@users.noreply.github.com>
2025-01-07 08:27:01 +0500

    EHR-11512-after-clicking-on-clear-button-on-quick-note-it-should-show… (#981)
    …-confirmation-alert
    
    Co-authored-by: Salman Rana <161335750+salman-rana-psychplus@users.noreply.github.com>

--- apps/galaxy/src/ui/quicknotes/quicknotes-clear-button.tsx
+++ apps/galaxy/src/ui/quicknotes/quicknotes-clear-button.tsx
@@ -1,20 +1,52 @@
 'use client'
 
-import { Button } from '@radix-ui/themes'
+import React from 'react'
+import { Button, Dialog, Flex, Text } from '@radix-ui/themes'
+import { TriangleAlert, X } from 'lucide-react'
 
 const QuickNotesClearButton = () => {
   return (
-    <Button
-      onClick={() => {
-        window.postMessage({ type: 'quicknotes:clear' }, '*')
-      }}
-      variant="outline"
-      color="gray"
-      size="1"
-      className="text-black"
-    >
-      Clear
-    </Button>
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <Button variant="outline" color="gray" size="1" className="text-black">
+          Clear
+        </Button>
+      </Dialog.Trigger>
+      <Dialog.Content className="bg-pp-warning-bg relative max-w-[440px] rounded-1 border-2 border-amber-10 p-4 pb-5 [box-shadow:none]">
+        <Dialog.Close className="absolute right-4 top-4 cursor-pointer">
+          <X size={20} strokeWidth={1.5} />
+        </Dialog.Close>
+
+        <Flex direction="row" gap="3" align="start">
+          <TriangleAlert className="min-w-6 text-amber-9" size={24} />
+          <Flex direction="column" gap="1" pt="1">
+            <Dialog.Title size="4" className="m-0 font-medium">
+              Clear Quick Notes
+            </Dialog.Title>
+            <Text as="p" size="2" mt="2">
+              Are you sure you want to clear quick notes ?
+            </Text>
+            <Flex justify="start" width="100%" gap="2" mt="3">
+              <Dialog.Close>
+                <Button className="border-pp-gray-2 text-pp-black-3 bg-white w-[166px] cursor-pointer border border-solid">
+                  <Text size="2">Cancel</Text>
+                </Button>
+              </Dialog.Close>
+              <Dialog.Close>
+                <Button
+                  onClick={() => {
+                    window.postMessage({ type: 'quicknotes:clear' }, '*')
+                  }}
+                  className={`bg-pp-link-text text-white w-[166px] cursor-pointer`}
+                >
+                  <Text size="2">Proceed</Text>
+                </Button>
+              </Dialog.Close>
+            </Flex>
+          </Flex>
+        </Flex>
+      </Dialog.Content>
+    </Dialog.Root>
   )
 }
 
