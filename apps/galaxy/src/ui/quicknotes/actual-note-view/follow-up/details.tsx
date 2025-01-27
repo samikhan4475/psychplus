1cc263f3284093191bb404d0e9ff015a59cefce0
Salman Rana <161335750+salman-rana-psychplus@users.noreply.github.com>
2025-01-27 23:09:42 +0500

    EHR-12082-fix (#1175)
    https://psychplus.atlassian.net/browse/EHR-12082
    
    Co-authored-by: salmandayal <salmanrana888@gmail.com>

--- apps/galaxy/src/ui/quicknotes/actual-note-view/follow-up/details.tsx
+++ apps/galaxy/src/ui/quicknotes/actual-note-view/follow-up/details.tsx
@@ -1,6 +1,6 @@
 import { Text } from '@radix-ui/themes'
 import { Appointment } from '@/types'
-import { formatDateTime } from '@/utils'
+import { formatDateCell, formatTimeCell } from '@/ui/schedule/utils'
 import { BlockContainer } from '../shared'
 
 const Details = ({ data }: { data: Appointment[] }) => {
@@ -8,8 +8,9 @@ const Details = ({ data }: { data: Appointment[] }) => {
     <BlockContainer heading="Follow Up in weeks">
       {data.map((row) => (
         <Text size="1" key={row.appointmentDate}>
-          {formatDateTime(row.appointmentDate, false)} | {row.providerName} |{' '}
-          {row.locationName} | {row.visitType}
+          {formatDateCell(row.appointmentDate, row.locationTimezoneId)}{' '}
+          {formatTimeCell(row.appointmentDate, row.locationTimezoneId)} |{' '}
+          {row.providerName} | {row.locationName} | {row.visitType}
         </Text>
       ))}
     </BlockContainer>
