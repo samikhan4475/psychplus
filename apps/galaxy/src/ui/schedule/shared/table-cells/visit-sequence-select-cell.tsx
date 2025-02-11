e60c5320f0e6c3182c11a05548d3a2d9f82d170d
Adnan Irfan <adnanshurta@gmail.com>
2025-02-11 21:04:23 +0500

    EHR-12401 Removed visit sequence select dropdown form list view for timed visits (#1304)
    [Ticket](https://psychplus.atlassian.net/browse/EHR-12401)

--- apps/galaxy/src/ui/schedule/shared/table-cells/visit-sequence-select-cell.tsx
+++ apps/galaxy/src/ui/schedule/shared/table-cells/visit-sequence-select-cell.tsx
@@ -1,13 +1,20 @@
-import { PropsWithRow } from '@/components'
+import { PropsWithRow, TextCell } from '@/components'
+import { CODESETS } from '@/constants'
+import { useCodesetCodes } from '@/hooks'
 import { Appointment } from '@/types'
-import { TimedVisitSequenceSelect } from './timed-visit-sequence-select'
+import { getCodesetDisplayName } from '@/utils'
 import { NonTimedVisitSequenceSelect } from './nontimed-visit-sequence-select'
 
 const VisitSequenceSelectCell = ({
   row: { original: appointment },
 }: PropsWithRow<Appointment>) => {
+  const codes = useCodesetCodes(CODESETS.VisitSequenceType)
   if (appointment.isServiceTimeDependent) {
-    return <TimedVisitSequenceSelect appointment={appointment} />
+    return (
+      <TextCell>
+        {getCodesetDisplayName(appointment.visitSequence, codes)}
+      </TextCell>
+    )
   }
   return <NonTimedVisitSequenceSelect appointment={appointment} />
 }
