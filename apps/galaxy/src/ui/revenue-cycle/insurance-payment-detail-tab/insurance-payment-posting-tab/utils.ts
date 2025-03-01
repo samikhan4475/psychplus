161b245fb9eaa93ea6c35b6c705f661417c07c7c
Muhammad Danish <mdanish@psychplus.com>
2025-03-01 08:07:23 +0500

    EHR-12726-insurance-payment-check-amount-amount-validation-skip-the-adjustment-from-paid-amount (#1448)

--- apps/galaxy/src/ui/revenue-cycle/insurance-payment-detail-tab/insurance-payment-posting-tab/utils.ts
+++ apps/galaxy/src/ui/revenue-cycle/insurance-payment-detail-tab/insurance-payment-posting-tab/utils.ts
@@ -5,102 +5,50 @@ interface ValidatePaymentParams {
   paymentDetail: InsurancePayment
   claimPayment: Partial<ClaimPayment>
 }
+const validatePayment = ({ claimPayment, paymentDetail }: ValidatePaymentParams): string => {
+  if (!claimPayment.claimServiceLinePayments) return '';
 
-const validatePayment = ({
-  claimPayment,
-  paymentDetail,
-}: ValidatePaymentParams): string => {
-  if (!claimPayment.claimServiceLinePayments) return ''
 
-  // validation for sum of all the amounts should not exceed the checkamount
-  const checkAmount = paymentDetail.amount
+  const checkAmount = paymentDetail.amount;
+  let totalPaid = 0;
+  const serviceLineAmountExceeded: number[] = [];
+  const amountAdjustedEqually: number[] = [];
 
-  let checkAmountExceeded = ''
-  let totalPaid = 0
-  for (const serviceLine of claimPayment.claimServiceLinePayments) {
-    if (!serviceLine.serviceLinePaymentAdjustments) continue
+  for (const [index, serviceLine] of claimPayment.claimServiceLinePayments.entries()) {
+    if (!serviceLine.serviceLinePaymentAdjustments) continue;
 
     const sumOfAdjustments = serviceLine.serviceLinePaymentAdjustments.reduce(
-      (acc, adj) =>
-        adj.recordStatus !== 'Inactive' ? acc + adj.adjustmentAmount : acc,
-      0,
-    )
-    const paidAmount = parseFloat(serviceLine.paidAmount)
-    const sumOfPaid = sumOfAdjustments + paidAmount
+      (acc, adj) => (adj.recordStatus !== 'Inactive' ? acc + adj.adjustmentAmount : acc),
+      0
+    );
+    
+    const allowedAmount = +serviceLine.allowedAmount;
+    const billedAmount = parseFloat(serviceLine.billedAmount);
+    const paidAmount = parseFloat(removeNegative(serviceLine.paidAmount));
+    const totalPaidForServiceLine = +removeNegative(`${sumOfAdjustments}`) + paidAmount;
 
-    totalPaid += sumOfPaid
+    totalPaid += paidAmount;
+    if (totalPaid > checkAmount) return 'Sum of all the amounts in service lines should not exceed the check amount';
 
-    if (totalPaid > checkAmount) {
-      checkAmountExceeded = `Sum of all the amounts in service lines should not exceed the check amount`
-      break
+    if (totalPaidForServiceLine > billedAmount && allowedAmount) {
+      serviceLineAmountExceeded.push(index + 1);
     }
-  }
 
-  if (checkAmountExceeded) return checkAmountExceeded
-
-  // validation for service lines the sum of all amounts should not exceed the billed amount
-  const serviceLineAmountExceeded: number[] = []
-
-  for (const [
-    index,
-    serviceLine,
-  ] of claimPayment.claimServiceLinePayments.entries()) {
-    if (!serviceLine.serviceLinePaymentAdjustments) continue
-
-    const sumOfAdjustments = serviceLine.serviceLinePaymentAdjustments.reduce(
-      (acc, adj) =>
-        adj.recordStatus !== 'Inactive' ? acc + adj.adjustmentAmount : acc,
-      0,
-    )
-    const allowedAmount = +serviceLine.allowedAmount
-    const billedAmount = parseFloat(serviceLine.billedAmount)
-    const paidAmount = parseFloat(removeNegative(serviceLine.paidAmount))
-    const totalPaid = +removeNegative(`${sumOfAdjustments}`) + paidAmount
-
-    if (totalPaid > billedAmount && allowedAmount) {
-      serviceLineAmountExceeded.push(index + 1)
-    }
-  }
-
-  if (serviceLineAmountExceeded.length > 0)
-    return `Sum of all the amounts should not exceed billed amount - Service Line (${serviceLineAmountExceeded.join(
-      ', ',
-    )})`
-
-  // need to check if sum of all the amounts are equal to billed amount
-  const amountAdjustedEqually: number[] = []
-
-  for (const [
-    index,
-    serviceLine,
-  ] of claimPayment.claimServiceLinePayments.entries()) {
-    if (!serviceLine.serviceLinePaymentAdjustments) continue
-
-    const sumOfAdjustments =
-      serviceLine.serviceLinePaymentAdjustments.reduce(
-        (acc, adj) =>
-          adj.recordStatus !== 'Inactive' ? acc + adj.adjustmentAmount : acc,
-        0,
-      ) ?? 0
-
-    const allowedAmount = +serviceLine.allowedAmount
-    const billedAmount = parseFloat(serviceLine.billedAmount)
-    const sumOfAmounts =
-      +removeNegative(`${sumOfAdjustments}`) +
-      +removeNegative(serviceLine.paidAmount) +
-      +removeNegative(serviceLine.otherPr)
-
+    const sumOfAmounts = totalPaidForServiceLine + +removeNegative(serviceLine.otherPr);
     if (sumOfAmounts.toFixed(2) !== billedAmount.toFixed(2) && allowedAmount) {
-      amountAdjustedEqually.push(index + 1)
+      amountAdjustedEqually.push(index + 1);
     }
   }
 
-  if (amountAdjustedEqually.length > 0)
-    return `Billed Amount is not adjusted properly - Service Line (${amountAdjustedEqually.join(
-      ', ',
-    )})`
+  if (serviceLineAmountExceeded.length > 0) {
+    return `Sum of all the amounts should not exceed billed amount - Service Line (${serviceLineAmountExceeded.join(', ')})`;
+  }
 
-  return ''
-}
+  if (amountAdjustedEqually.length > 0) {
+    return `Billed Amount is not adjusted properly - Service Line (${amountAdjustedEqually.join(', ')})`;
+  }
+
+  return '';
+};
 
 export { validatePayment }
