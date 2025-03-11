3aa9fe810b8d1c64fa5814a9b5e4dcc69f9d13ef
Daniyal Ahmed <124872459+daniyal094@users.noreply.github.com>
2025-03-11 22:27:07 +0500

    refactor: simplify SocialHx component by removing unused note data re… (#1523)
    …trieval

--- apps/patient/src/features/pre-checkin-assessment/ui/pre-checkin-assessment-stepper/steps/histories/blocks/social-hx/social-hx.tsx
+++ apps/patient/src/features/pre-checkin-assessment/ui/pre-checkin-assessment-stepper/steps/histories/blocks/social-hx/social-hx.tsx
@@ -2,32 +2,14 @@
 
 import React from 'react'
 import { SocialHxView } from '@/features/histories/ui'
-import { NoteSectionName } from '@/features/note/constants'
-import { useNoteStore } from '@/features/note/store'
-import { NoteSectionItem } from '@/features/note/types'
 import { NoteAccordion, NoteAccordionContentProps } from '@/features/note/ui'
 
 const SocialHx = () => {
-  const { getNoteData } = useNoteStore((state) => ({
-    getNoteData: state.getNoteData,
-  }))
-  const data = getNoteData(NoteSectionName.NoteSectionSocialHx)
-
-  return (
-    <NoteAccordion
-      title="Social Hx"
-      data={data}
-      content={renderSocialHx}
-      sectionName={NoteSectionName.NoteSectionSocialHx}
-    />
-  )
+  return <NoteAccordion title="Social Hx" content={renderSocialHx} />
 }
 
-const renderSocialHx = ({
-  handleSave,
-  isEdit,
-}: NoteAccordionContentProps<NoteSectionItem>) => (
-  <SocialHxView onSave={handleSave} isEdit={isEdit} />
+const renderSocialHx = ({ handleSave }: NoteAccordionContentProps) => (
+  <SocialHxView onSave={handleSave} />
 )
 
 export { SocialHx }
