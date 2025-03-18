c3d98c9f78d0b80e1405f08c8b41162d5665db6a
Talha Manzoor <156697102+talha-psychplus@users.noreply.github.com>
2025-03-18 07:28:32 +0500

    EHR-13419 : Invalid  Error handling for Allergies Medications and Pha… (#1553)
    …rmacies

--- apps/patient/src/features/pre-checkin-assessment/ui/pre-checkin-assessment-view.tsx
+++ apps/patient/src/features/pre-checkin-assessment/ui/pre-checkin-assessment-view.tsx
@@ -31,7 +31,7 @@ const PreCheckinAssessmentView = async () => {
     codesets,
     profileResponse,
     pharmaciesResponse,
-    dawSystemFeatureFlagResponse,
+    // dawSystemFeatureFlagResponse,
     patientMedicationsResponse,
     patientAllergiesResponse,
   ] = await Promise.all([
@@ -49,7 +49,7 @@ const PreCheckinAssessmentView = async () => {
     ]),
     getProfile(),
     getPatientPharmacies(),
-    getIsFeatureFlagEnabled(FeatureFlags.ehr8973EnableDawMedicationApi),
+    // getIsFeatureFlagEnabled(FeatureFlags.ehr8973EnableDawMedicationApi), commenting this out for 2nd phase
     getPatientMedications(),
     getPatientAllergies(),
   ])
@@ -70,21 +70,16 @@ const PreCheckinAssessmentView = async () => {
     return <Text>{profileResponse.error}</Text>
   }
 
-  if (pharmaciesResponse.state === 'error') {
-    return <Text>{pharmaciesResponse.error}</Text>
-  }
-
-  if (dawSystemFeatureFlagResponse.state === 'error') {
-    return <Text>{dawSystemFeatureFlagResponse.error}</Text>
-  }
-
-  if (patientMedicationsResponse.state === 'error') {
-    return <Text>{patientMedicationsResponse.error}</Text>
-  }
-
-  if (patientAllergiesResponse.state === 'error') {
-    return <Text>{patientAllergiesResponse.error}</Text>
-  }
+  const pharmacies =
+    pharmaciesResponse.state === 'error' ? [] : pharmaciesResponse.data
+  const medications =
+    patientMedicationsResponse.state === 'error'
+      ? []
+      : patientMedicationsResponse.data
+  const allergies =
+    patientAllergiesResponse.state === 'error'
+      ? []
+      : patientAllergiesResponse.data
 
   const questionnaireDashboardResponse = await getNoteDetails({
     patientId: profileResponse.data.id,
@@ -133,31 +128,31 @@ const PreCheckinAssessmentView = async () => {
       <GooglePlacesContextProvider apiKey={GOOGLE_MAPS_API_KEY}>
         <CodesetStoreProvider codesets={codesets}>
           <NoteStoreProvider notes={noteDetailsResponse.data}>
-          <Box className="mx-auto w-[1200px]" mt="9">
-            <PreCheckinAssessmentStapper
-              insurancePayers={insurancePayerResponse.data}
-              patientInsurances={patientInsurancesResponse.data}
-              creditCards={creditCardsResponse.data}
-              stripeAPIKey={STRIPE_PUBLISHABLE_KEY}
-              pharmacies={pharmaciesResponse.data}
-              medications={patientMedicationsResponse.data}
-              allergies={patientAllergiesResponse.data}
-              isDawSystemFeatureFlagEnabled={true} //Currently we have to remove DAW system feature flag dependency
-              questionnaireSectionsToShowOnPreCheckin={
-                questionnaireSectionsToShowOnPreCheckin
-              }
-              preCheckInProgress={{
-                preCheckInCompletedTabs:
-                  preCheckInProgress?.content?.preCheckInCompletedTabs,
-                isPreCheckInCompleted:
-                  preCheckInProgress?.content?.isPreCheckInCompleted,
-                activeTab: preCheckInProgress?.content?.activeTab,
-                id: String(preCheckInProgress?.id),
-              }}
-            />
-          </Box>
-        </NoteStoreProvider>
-      </CodesetStoreProvider>
+            <Box className="mx-auto w-[1200px]" mt="9">
+              <PreCheckinAssessmentStapper
+                insurancePayers={insurancePayerResponse.data}
+                patientInsurances={patientInsurancesResponse.data}
+                creditCards={creditCardsResponse.data}
+                stripeAPIKey={STRIPE_PUBLISHABLE_KEY}
+                pharmacies={pharmacies}
+                medications={medications}
+                allergies={allergies}
+                isDawSystemFeatureFlagEnabled={true} //Currently we have to remove DAW system feature flag dependency
+                questionnaireSectionsToShowOnPreCheckin={
+                  questionnaireSectionsToShowOnPreCheckin
+                }
+                preCheckInProgress={{
+                  preCheckInCompletedTabs:
+                    preCheckInProgress?.content?.preCheckInCompletedTabs,
+                  isPreCheckInCompleted:
+                    preCheckInProgress?.content?.isPreCheckInCompleted,
+                  activeTab: preCheckInProgress?.content?.activeTab,
+                  id: String(preCheckInProgress?.id),
+                }}
+              />
+            </Box>
+          </NoteStoreProvider>
+        </CodesetStoreProvider>
       </GooglePlacesContextProvider>
     </ProfileStoreProvider>
   )
