c826c9591cd6ab212412db991b460f189f742256
Ahtsham Rao <54804098+Ahtxham@users.noreply.github.com>
2025-04-22 21:01:56 +0500

    Add copy link functionality and welcome message in CallView component (#1760)

--- apps/galaxy/src/ui/call/call-view.tsx
+++ apps/galaxy/src/ui/call/call-view.tsx
@@ -2,11 +2,13 @@
 
 import { useCallback, useEffect, useState } from 'react'
 import { CallAdapterState } from '@azure/communication-react'
-import { Flex, Text } from '@radix-ui/themes'
+import { Button, Flex, Heading, Text, TextField } from '@radix-ui/themes'
+import { CopyIcon } from 'lucide-react'
 import toast from 'react-hot-toast'
 import { MAIN_PAGE_FEATURE_FLAGS } from '@/constants'
 import { useFeatureFlagEnabled } from '@/hooks/use-feature-flag-enabled'
 import { webSocketEventBus } from '@/lib/websocket-event-bus'
+import { useStore } from '@/store'
 import { WebSocketEvents, WebSocketEventType } from '@/types'
 import { AppointmentsList, CallCompositeContainer } from './blocks'
 import { AcsInfo } from './types'
@@ -16,6 +18,8 @@ interface Props {
 }
 
 const CallView = ({ acsInfo }: Props) => {
+  const user = useStore((state) => state.user)
+
   const isAvfeatureFlagEnabled = useFeatureFlagEnabled(
     MAIN_PAGE_FEATURE_FLAGS.ehr9475AudioVideoTelemedicine,
   )
@@ -29,6 +33,7 @@ const CallView = ({ acsInfo }: Props) => {
   >([])
   const [uniqueGvs] = useState(() => new Set<string>())
 
+  const providerLink = `${window.location.origin}/call?staffId=${user?.staffId}`
   const updateAppointments = useCallback(
     (message: WebSocketEvents[WebSocketEventType.CallWaiting]) => {
       if (uniqueGvs.has(message.gv)) {
@@ -71,6 +76,12 @@ const CallView = ({ acsInfo }: Props) => {
     }
   }, [appointmentId, callAdapterState, removeAppointment])
 
+  const handleCopy = () => {
+    navigator.clipboard.writeText(providerLink).then(() => {
+      toast.success('Copied to clipboard')
+    })
+  }
+
   if (!isAvfeatureFlagEnabled) {
     return <Text>Feature not enabled</Text>
   }
@@ -83,28 +94,56 @@ const CallView = ({ acsInfo }: Props) => {
         setAppointmentId={setAppointmentId}
         callAdapterState={callAdapterState}
       />
-
-      <Flex
-        direction="column"
-        justify="center"
-        align="center"
-        width={'80vw'}
-        height={'100%'}
-      >
-        {appointmentId ? (
-          <CallCompositeContainer
-            acsInfo={acsInfo}
-            appointmentId={appointmentId}
-            setCallAdapterState={setCallAdapterState}
-          />
-        ) : (
-          <Text>
-            {appointments.length
-              ? 'Press the "Join" button to start the call.'
-              : 'There are currently no patients in the queue.'}
-          </Text>
-        )}
-      </Flex>
+      {appointmentId ? (
+        <CallCompositeContainer
+          acsInfo={acsInfo}
+          appointmentId={appointmentId}
+          setCallAdapterState={setCallAdapterState}
+        />
+      ) : (
+        <Flex direction={'column'} flexGrow={'1'}>
+          <Flex
+            py="3"
+            className="bg-white h-fit px-2.5 shadow-2"
+            direction="column"
+            m="3"
+            width={'50vw'}
+          >
+            <Flex direction="column" mb="4" gap="1">
+              <Heading>
+                Welcome, {user?.legalName.firstName}. {user?.legalName.lastName}
+              </Heading>
+              <Text color="gray">
+                Please invite someone to your waiting room, share this link
+              </Text>
+            </Flex>
+            <Flex gap="1">
+              <TextField.Root
+                size="2"
+                value={providerLink}
+                disabled
+                className="w-full"
+              />
+              <Button size="2" highContrast onClick={handleCopy}>
+                <CopyIcon size="20" />
+                <Text>Copy</Text>
+              </Button>
+            </Flex>
+          </Flex>
+          <Flex
+            width={'100%'}
+            height={'80%'}
+            align={'center'}
+            justify={'center'}
+          >
+            <Text>
+              {appointments.length
+                ? 'Press the "Join" button to start the call.'
+                : 'There are currently no patients in the queue.'}
+            </Text>
+          </Flex>
+        </Flex>
+      )}
     </Flex>
   )
 }
