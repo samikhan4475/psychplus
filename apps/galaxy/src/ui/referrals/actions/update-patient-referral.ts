93d7c1df39483ed3d869fef12b1d1edbc462de8c
Hussnain Jutt <hjutt@psychplus.com>
2025-04-29 18:44:03 +0500

    EHR-14215-edit-referral-error (#1810)

--- apps/galaxy/src/ui/referrals/actions/update-patient-referral.ts
+++ apps/galaxy/src/ui/referrals/actions/update-patient-referral.ts
@@ -2,13 +2,24 @@
 
 import * as api from '@/api'
 import { PatientReferral } from '@/types'
+import { pickFields } from '@/utils'
 
 const updatePatientReferralAction = async (
   referral: PatientReferral,
 ): Promise<api.ActionResult<PatientReferral>> => {
+  const payload = pickFields(referral, [
+    'service',
+    'servicesStatus',
+    'contactStatus',
+    'resourceStatus',
+    'comments',
+    'referredByName',
+    'id',
+    'patientId',
+  ])
   const response = await api.PUT<PatientReferral>(
     api.UPDATE_PATIENT_REFERRAL_ENDPOINT(referral.patientId, referral.id),
-    referral,
+    payload,
   )
   if (response.state === 'error') {
     return {
