1cf51d5f2b5b2d4878cf58fa6a3fd1c5ac2e394f
aaqibrasool <arasool@psychplus.com>
2025-07-19 07:57:02 +0500

    resetting the default value on clear action (#2180)

--- apps/galaxy/src/ui/clinic-schedule/clinic-time-tab/clinic-time-filter-form.tsx
+++ apps/galaxy/src/ui/clinic-schedule/clinic-time-tab/clinic-time-filter-form.tsx
@@ -46,7 +46,6 @@ const ClinicTimeFilterForm = ({ staffId }: PropsWithStaffId) => {
 
   const handleClear = () => {
     form.reset()
-    form.setValue('scheduleStatus', '')
     fetchClinicSchedules(staffId)
   }
 
