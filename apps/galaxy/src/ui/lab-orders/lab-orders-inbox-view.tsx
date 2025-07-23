cbc1536c6948d6be421592da4069de29cbab3618
BilalAslampsychplus <baslam@psychplus.com>
2025-07-23 07:28:06 +0500

    EHR-15609-inbox-lab-order-filter-issue:done (#2212)

--- apps/galaxy/src/ui/lab-orders/lab-orders-inbox-view.tsx
+++ apps/galaxy/src/ui/lab-orders/lab-orders-inbox-view.tsx
@@ -3,6 +3,8 @@
 import { useEffect } from 'react'
 import { Flex } from '@radix-ui/themes'
 import { WidgetContainer } from '@/components'
+import { STAFF_ROLE_CODE_PRESCRIBER } from '@/constants'
+import { useStore as useGlobalStore } from '@/store'
 import { LabOrderStatusEnum } from './add-lab-order/blocks/types'
 import { AddLabOrdersButton } from './lab-orders-widget/add-lab-orders-button'
 import { LabOrderTablePagination } from './lab-orders-widget/lab-order-pagination-table'
@@ -11,19 +13,24 @@ import { LabOrdersFilterForm } from './lab-orders-widget/lab-orders-filter-form'
 import { ReviewAllButton } from './lab-orders-widget/review-all-button'
 import { useStore } from './lab-orders-widget/store'
 
-const labOrderPayload = {
-  orderStatus: LabOrderStatusEnum.Unsigned,
-  IsOrderSendStatus: false,
-  resourceStatusList: ['Active'],
-}
-
 const LabOrdersInboxView = () => {
   const { fetch, setAppointmentId, data } = useStore((state) => ({
     fetch: state.fetch,
     setAppointmentId: state.setAppointmentId,
     data: state.data,
   }))
-
+  const { staffId, staffRoleCode } = useGlobalStore((state) => ({
+    staffId: state.user.staffId,
+    staffRoleCode: state.staffResource.staffRoleCode,
+  }))
+  const isPrescriber = staffRoleCode === STAFF_ROLE_CODE_PRESCRIBER
+  const defaultOrderingStaffId = isPrescriber ? String(staffId) : ''
+  const labOrderPayload = {
+    orderStatus: LabOrderStatusEnum.Unsigned,
+    IsOrderSendStatus: null,
+    resourceStatusList: ['Active'],
+    orderingStaffId: defaultOrderingStaffId,
+  }
   const fetchData = () => {
     setAppointmentId(null)
     fetch(null, labOrderPayload)
