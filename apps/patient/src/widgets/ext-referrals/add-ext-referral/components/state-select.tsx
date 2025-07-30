09101b2611e37ffccb8b8cb99cb415ebb0c3699e
Hussnain Jutt <hjutt@psychplus.com>
2025-07-31 02:21:11 +0500

    Ehr 15833 auto select state referral (#2261)

--- apps/patient/src/widgets/ext-referrals/add-ext-referral/components/state-select.tsx
+++ apps/patient/src/widgets/ext-referrals/add-ext-referral/components/state-select.tsx
@@ -1,26 +1,47 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
+import { useSearchParams } from 'next/navigation'
 import { CODESETS } from '@psychplus-v2/constants'
 import { useFormContext } from 'react-hook-form'
 import { CodesetFormSelect, FormField } from '@/components-v2'
 import { useUserLocation } from '@/hooks/use-user-location'
+import { useCodesetCodes } from '@/providers'
 import { SchemaType } from './schema'
 
 interface Props {
   googleAPIkey: string
 }
 const StateSelect = ({ googleAPIkey }: Props) => {
+  const stateCodes = useCodesetCodes(CODESETS.UsStates)
   const { setValue } = useFormContext<SchemaType>()
 
-  const { zip, state } = useUserLocation(googleAPIkey)
+  const { zip, state: detectedState } = useUserLocation(googleAPIkey)
+
+  const searchParams = useSearchParams()
+  const stateNameFromQuery = searchParams?.get('stateName')?.toLowerCase()
+  
+  const resolvedStateCode = useMemo(() => {
+    if (!stateNameFromQuery || !stateCodes) return null
+
+    const match = stateCodes.find(
+      (code) => code.display?.toLowerCase() === stateNameFromQuery,
+    )
+
+    return match?.value ?? null
+  }, [stateNameFromQuery, stateCodes])
+
   useEffect(() => {
-    if (!state) return
-    setValue('patientContactDetails.addresses.0.state', state)
+    const effectiveState = resolvedStateCode || detectedState
+
+    if (!effectiveState) return
+
+    setValue('patientContactDetails.addresses.0.state', effectiveState)
+
     if (zip) {
       setValue('patientContactDetails.addresses.0.postalCode', zip)
     }
-  }, [state, setValue])
+  }, [resolvedStateCode, detectedState, zip, setValue])
   return (
     <FormField
       containerClassName="flex-1"
